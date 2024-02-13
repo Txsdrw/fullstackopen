ebf01aebd835de1d0a153e5b58a907e87678cfd3
txs <toxicusdraw@gmail.com>
2024-02-13 23:34:22 +0600

    ex 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -20,28 +20,30 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total total={parts.reduce((acc, item) => acc + item.exercises, 0)} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.parts.reduce((acc, item) => acc + item.exercises, 0)} />
     </div>
   );
 };
