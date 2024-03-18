1208e7dcfa43575f574af8b1132d235bf0e7e8de
txs <toxicusdraw@gmail.com>
2024-03-18 21:04:01 +0500

    ex 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,34 +1,4 @@
-const Header = ({course}) => {
-  return <h1>{course}</h1>;
-};
-
-const Content = ({parts}) => {
-  return (
-    <>
-    {parts.map((part, index) =>  <Part key={index} name={part.name} exercises={part.exercises} />)}
-    </>
-  );
-};
-
-const Part = (props) => {
-  return <p>{props.name} {props.exercises}</p>
-}
-
-const Total = ({parts}) => {
-  return (
-    <p>Total of {parts.reduce((total, part) => total + part.exercises, 0)} exercises</p>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  )
-}
+import { Course } from "./Course"
 
 const App = () => {
   const courses = [
